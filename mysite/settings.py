579fd598228853820b07469841d5ce909958dc55
gywls517 <gywls517@naver.com>
2019-09-22 14:26:45 +0900

    DB changed from SQLite to MySQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+          'NAME': 'hellodjango',
+          'HOST': 'localhost',
+          'PORT': '3306',
+          'USER': 'root',
+          'PASSWORD': 'jini0517'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
